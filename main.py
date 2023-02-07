dfb6853a7d0070e5fb3fbae5135c6412285de3e1
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-07 17:13:13 +0300

    Fix errors.
    1. fixed variable names
    2. fixed functions
    3. fixed network requests
    4. comments removed
    5. added division by zero checks

--- main.py
+++ main.py
@@ -8,139 +8,135 @@ from dotenv import load_dotenv
 import tools
 
 
-#---------------------- HEADHUNTER ------------------------
-
-def fetch_areas_ids_hh(area):
+def fetch_area_id_hh(area):
     """Возвращает результат поиска (словарь IDs) территорий России по справочнику hh.ru или None"""
+    hh_url = "https://api.hh.ru/areas"
+    headers = {
+        'User-Agent': 'App/1.0',
+    }
+    response = requests.get(url=hh_url, headers=headers)
+    response.raise_for_status()
+    rus_areas = response.json()[0]['areas']
     if area:
-        hh_url = "https://api.hh.ru/areas"
-        headers = {
-            'User-Agent': 'App/1.0',
-        }
-        response = requests.get(url=hh_url, headers=headers)
-        response.raise_for_status()
-        rus_areas = response.json()[0]['areas']
-        areas_ids = [row['id'] for row in rus_areas if area.lower() in row['name'].lower()]
-        return areas_ids
-    else:
-        return None
+        area_id = [row['id'] for row in rus_areas if area.lower() in row['name'].lower()]
+        return area_id
 
 
-def fetch_vacancy_hh(vacancy, area=None, period=None, page=None):
+def fetch_vacancy_hh(language, area=None, period=None, page=None):
     """Возвращает ответ на запрос по словарю вакансий или вызывает исключение"""
-    areas_ids = None
-    if area:
-        if area.isalpha():
-            areas_ids = fetch_areas_ids_hh(area)
-
+    area_id = fetch_area_id_hh(area)
     hh_url = "https://api.hh.ru/vacancies"
     headers = {
         'User-Agent': 'App/1.0',
     }
-
     payload = {
         'host': 'hh.ru',
-        'text': vacancy,
-        'area': areas_ids,
+        'text': language,
+        'area': area_id,
         'period': period,
         'page': page,
         'only_with_salary': True,
         'currency': 'RUR',
     }
     response = requests.get(url=hh_url, params=payload, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    response.raise_for_status()
+    return response.json()
 
 
-def predict_rub_salary_hh(vacancy, area=None, period=None):
+def predict_rub_salary_hh(language, area=None, period=None):
     """Возвращает средний размер зарплаты по заданной вакансии на hh.ru"""
     vacancy_salaries = []
-    first_response = fetch_vacancy_hh(vacancy, area=area, period=period)
-    vacancies_found = first_response['found']
-    pages_number = first_response['pages']
-    if vacancies_found > 0:
-        page = 0
-        while page < int(pages_number):
-            page_response = fetch_vacancy_hh(vacancy, area=area, period=period, page=page)
-            if page_response:
-                for salary in page_response['items']:
-                    if salary['salary']['currency'] == 'RUR':
-                        rub_salary = tools.compute_average_salary(salary_from=salary['salary']['from'],
-                                                                  salary_to=salary['salary']['to'])
-                        vacancy_salaries.append(rub_salary)
-            page += 1
-            time.sleep(1)
+    page = 0
+    pages_count = 1
+    while page < pages_count:
+        try:
+            page_response = fetch_vacancy_hh(language, area=area, period=period, page=page)
+            vacancies_found = page_response['found']
+            pages_count = int(page_response['pages'])
+            for vacancy in page_response['items']:
+                currency = vacancy['salary']['currency']
+                salary_from = vacancy['salary']['from']
+                salary_to = vacancy['salary']['to']
+                if currency == 'RUR' and (salary_from != '' or salary_to != ''):
+                    rub_salary = tools.compute_average_salary(salary_from, salary_to)
+                    vacancy_salaries.append(rub_salary)
+        except requests.exceptions.HTTPError as err:
+            print(f"Page {page}: {err}")
+        page += 1
+        time.sleep(1)
+
+    average_salary = 0
+    vacancies_processed = 0
+
+    if len(vacancy_salaries) > 0:
         vacancies_processed = len(vacancy_salaries)
         average_salary = int(sum(vacancy_salaries) / vacancies_processed)
-    else:
-        vacancies_processed = 0
-        average_salary = 0
 
-    predict = dict(vacancies_found=vacancies_found,
+    dataset = dict(vacancies_found=vacancies_found,
                    vacancies_processed=vacancies_processed,
                    average_salary=average_salary)
-    return predict
+    return dataset
 
-#----------------------- SUPERJOB --------------------------
 
-def fetch_vacancy_sj(vacancy, area=None, period=0, page=None):
+def fetch_vacancy_sj(sj_key, language, area=None, period=0, page=None):
     """Возвращает ответ сайта SuperJob.ru по заданным параметрам"""
-    load_dotenv()
-    sj_key = os.environ.get('SUPERJOB_SECRET_KEY')
     sj_url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id': sj_key,
     }
     payload = {
-        'keyword': vacancy,
+        'keyword': language,
         'town': area,
         'period': period,
         'page': page,
         'no_agreement': 1,
     }
     response = requests.get(url=sj_url, params=payload, headers=headers)
-    # response.raise_for_status()
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    response.raise_for_status()
+    return response.json()
 
 
-def predict_rub_salary_sj(vacancy, area=None, period=0):
+def predict_rub_salary_sj(sj_key, language, area=None, period=0):
     """Возвращает расчёт средней заработной платы по данным сайта SuperJob.ru"""
     vacancy_salaries = []
-    first_response = fetch_vacancy_sj(vacancy, area=area, period=period)
-    vacancies_found = first_response['total']
-    pages_number = round(int(vacancies_found) / 20 + 0.5)
-    if vacancies_found > 0:
-        page = 0
-        while page < int(pages_number):
-            page_response = fetch_vacancy_sj(vacancy, area=area, period=period, page=page)
-            if page_response:
-                for salary in page_response['objects']:
-                    if salary['currency'] == 'rub':
-                        rub_salary = tools.compute_average_salary(salary_from=salary['payment_from'],
-                                                                  salary_to=salary['payment_to'])
+    vacancies_found = 0
+    page = 0
+    pages_count = 1
+    while page < pages_count:
+        try:
+            page_response = fetch_vacancy_sj(sj_key, language, area=area, period=period, page=page)
+            if page_response['total'] > 0:
+                vacancies_found = page_response['total']
+            if len(page_response['objects']) > 0:
+                pages_count = round(int(vacancies_found)/len(page_response['objects']) + 0.5)
+                for vacancy in page_response['objects']:
+                    currency = vacancy['currency']
+                    salary_from = vacancy['payment_from']
+                    salary_to = vacancy['payment_to']
+                    if currency == 'rub' and (salary_from != '' or salary_to != ''):
+                        rub_salary = tools.compute_average_salary(salary_from, salary_to)
                         vacancy_salaries.append(rub_salary)
-            page += 1
-            time.sleep(1)
+        except requests.exceptions.HTTPError as err:
+            print(f"Page {page}: {err}")
+        page += 1
+        time.sleep(1)
+
+    average_salary = 0
+    vacancies_processed = 0
+
+    if len(vacancy_salaries) > 0:
         vacancies_processed = len(vacancy_salaries)
         average_salary = int(sum(vacancy_salaries) / vacancies_processed)
-    else:
-        vacancies_processed = 0
-        average_salary = 0
 
-    predict = dict(vacancies_found=vacancies_found,
+    dataset = dict(vacancies_found=vacancies_found,
                    vacancies_processed=vacancies_processed,
                    average_salary=average_salary)
-    return predict
+    return dataset
 
-#----------------------- MAIN ------------------------------
 
 def main():
-    vacancies = ['Python', 'С++', 'C#', 'Java', 'JavaScript', 'C',  'PHP', 'Swift', 'Go', 'Kotlin']
+    load_dotenv()
+    sj_key = os.environ.get('SUPERJOB_SECRET_KEY')
 
     parser = argparse.ArgumentParser(description='Поиск средней зарплаты по вакансиям')
     parser.add_argument('-a', '--area', default=None, help='Ввести наименование города')
@@ -149,25 +145,26 @@ def main():
     area = args.area
     period = args.period
 
+    table_title_hh = 'HeadHunter'
+    table_title_sj = 'SuperJob'
+
     if area:
         area = area.capitalize()
-        table_title_hh = f"HeadHunter {area}"
-        table_title_sj = f"SuperJob {area}"
-    else:
-        table_title_hh = 'HeadHunter'
-        table_title_sj = 'SuperJob'
+        table_title_hh = f'HeadHunter {area}'
+        table_title_sj = f'SuperJob {area}'
 
-    predict_hh = dict()
-    predict_sj = dict()
+    dataset_hh = dict()
+    dataset_sj = dict()
 
-    for vacancy in vacancies:
-        print(vacancy)
-        predict_hh[vacancy] = predict_rub_salary_hh(vacancy, area=area, period=period)
-        predict_sj[vacancy] = predict_rub_salary_sj(vacancy, area=area, period=period)
+    programming_languages = ['Python', 'С++', 'C#', 'Java', 'JavaScript', 'C', 'PHP', 'Swift', 'Go', 'Kotlin']
+
+    for language in programming_languages:
+        dataset_hh[language] = predict_rub_salary_hh(language, area=area, period=period)
+        dataset_sj[language] = predict_rub_salary_sj(sj_key, language, area=area, period=period)
         time.sleep(1)
 
-    tools.print_terminal_table(predict_hh, table_title_hh)
-    tools.print_terminal_table(predict_sj, table_title_sj)
+    tools.print_terminal_table(dataset_hh, table_title_hh)
+    tools.print_terminal_table(dataset_sj, table_title_sj)
 
 
 if __name__ == '__main__':
